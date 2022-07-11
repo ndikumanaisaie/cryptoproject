dd96821842eea8fca85af2b038709c101aca517b
ndikumanaisaie <ndikumanaisaie@gmail.com>
2022-07-11 21:31:31 +0100

    Adding footer

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable import/named */
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
@@ -8,28 +9,37 @@ import {
 	Navbar, Homepage, Exchanges, Cryptocurrencies, CryptoDetails, News,
 } from './components';
 
-const App = () => (
-	<div className="app">
-		<div className="navbar">
-			<Navbar />
-		</div>
-		<div className="main">
-			<Layout>
-				<div className="routes">
-					<Routes>
-						<Route path='/' element={<Homepage />} />
-						<Route path='/exchanges' element={<Exchanges />} />
-						<Route path='/cryptocurrencies' element={<Cryptocurrencies />} />
-						<Route path='/crypto/:coinId' element={<CryptoDetails />} />
-						<Route path='/news' element={<News />} />
-					</Routes>
+const App = () => {
+	const { Title } = Typography;
+	return (
+		<div className="app">
+			<div className="navbar">
+				<Navbar />
+			</div>
+			<div className="main">
+				<Layout>
+					<div className="routes">
+						<Routes>
+							<Route path='/' element={<Homepage />} />
+							<Route path='/exchanges' element={<Exchanges />} />
+							<Route path='/cryptocurrencies' element={<Cryptocurrencies />} />
+							<Route path='/crypto/:coinId' element={<CryptoDetails />} />
+							<Route path='/news' element={<News />} />
+						</Routes>
+					</div>
+				</Layout>
+				<div className="footer">
+					<Title level={5} style={{ color: 'white', textAlign: 'center' }}>
+						Cryptoverse <br /> All rights reserved
+					</Title>
+					<Space>
+						<Link to="/">Home</Link>
+						<Link to="/exchanges">Exchanges</Link>
+						<Link to="/news">News</Link>
+					</Space>
 				</div>
-			</Layout>
+			</div>
 		</div>
-		<div className="footer">
-
-		</div>
-	</div>
-);
-
+	);
+};
 export default App;
